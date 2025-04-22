afd90262accbe3af581bcc670cdb164eb4720567
Nizar <niarhakim@gmail.com>
2025-04-22 22:41:24 +0400

    Removed card.js and added card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,5 +1,8 @@
-
 export const Card = ({ children, className }) => (
   <div className={`bg-white rounded-xl shadow-md ${className}`}>{children}</div>
 );
-export const CardContent = ({ children }) => <div className="p-4">{children}</div>;
+
+export const CardContent = ({ children }) => (
+  <div className="p-4">{children}</div>
+);
+
